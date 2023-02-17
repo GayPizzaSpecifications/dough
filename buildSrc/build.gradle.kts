acd5ebbdb95b1905dbc83f61ea462ba368640dcb
Alex Zenla <alex@fork.run>
2023-02-17 13:24:40 -0800

    dough sample jar and run tasks

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,4 +9,6 @@ repositories {
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
   implementation("org.jetbrains.kotlin:kotlin-serialization:1.8.10")
+
+  implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
 }
--- buildSrc/src/main/kotlin/dough_sample.gradle.kts
+++ buildSrc/src/main/kotlin/dough_sample.gradle.kts
@@ -1,3 +1,34 @@
+import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
+
 plugins {
   id("dough_base")
+}
+
+val jvmMain = kotlin.targets.getByName<KotlinJvmTarget>("jvm").compilations.getByName("main")
+
+tasks {
+  val sampleJar = register<Jar>("sampleJar") {
+    group = "application"
+
+    manifest {
+      attributes["Main-Class"] = "MainKt"
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    archiveBaseName.set("${project.name}-sample")
+    from(jvmMain.output.classesDirs, jvmMain.compileDependencyFiles.map {
+      if (it.isDirectory) it else zipTree(it)
+    })
+  }
+
+  register<JavaExec>("runSample") {
+    group = "application"
+
+    mainClass.set("MainKt")
+    classpath = jvmMain.output.classesDirs
+    classpath += jvmMain.compileDependencyFiles
+  }
+
+  assemble.get().dependsOn(sampleJar)
 }
