ead67199fd0094cdceae028e3621b12e4371c1bc
Alex Zenla <alex@fork.run>
2023-09-05 16:02:01 -0700

    Publish to GitLab Package Registry

--- buildSrc/src/main/kotlin/dough_publishing.gradle.kts
+++ buildSrc/src/main/kotlin/dough_publishing.gradle.kts
@@ -13,12 +13,30 @@ publishing {
       githubPackagesToken = project.findProperty("github.token") as String?
     }
 
+    var gitlabPackagesToken = System.getenv("GITLAB_TOKEN")
+    if (gitlabPackagesToken == null) {
+      gitlabPackagesToken = project.findProperty("gitlab.com.accessToken") as String?
+    }
+
     maven {
       name = "GitHubPackages"
       url = uri("https://maven.pkg.github.com/gaypizzaspecifications/dough")
       credentials {
         username = project.findProperty("github.username") as String? ?: "gaypizzaspecifications"
         password = githubPackagesToken
+      }
+    }
+
+    maven {
+      name = "GitLab"
+      url = uri("https://gitlab.com/api/v4/projects/49101454/packages/maven")
+      credentials(HttpHeaderCredentials::class.java) {
+        name = "Private-Token"
+        value = gitlabPackagesToken
+      }
+
+      authentication {
+        create<HttpHeaderAuthentication>("header")
       }
     }
   }
